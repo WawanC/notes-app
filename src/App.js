b91e727f7d6b1a8aa9ce212a856f490b65e1dd7c
WawanC <kurniawancristianto@gmail.com>
2022-06-05 21:25:55 +0700

    add initial state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-const App = () => {
-  return <h1>Notes App</h1>;
-};
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+    };
+  }
+
+  render() {
+    return <h1>Notes App</h1>;
+  }
+}
 
 export default App;
