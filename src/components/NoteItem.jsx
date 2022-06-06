2f073a360b737035fdc29d964b8096df084fd22e
WawanC <kurniawancristianto@gmail.com>
2022-06-06 18:57:37 +0700

    improve icons

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,60 +1,6 @@
 import "../styles/NoteItem.css";
 import { showFormattedDate } from "../utils";
-
-const iconSize = 20;
-
-const ArchiveIcon = () => (
-  <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={iconSize}
-      height={iconSize}
-      fill="currentColor"
-      class="bi bi-archive"
-      viewBox="0 0 16 16"
-    >
-      <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
-    </svg>
-    <span className="tooltip">Archive Note</span>
-  </>
-);
-
-const MoveIcon = () => (
-  <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={iconSize}
-      height={iconSize}
-      fill="currentColor"
-      class="bi bi-folder-x"
-      viewBox="0 0 16 16"
-    >
-      <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0 0 13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91H9v1H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zm6.339-1.577A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z" />
-      <path d="M11.854 10.146a.5.5 0 0 0-.707.708L12.293 12l-1.146 1.146a.5.5 0 0 0 .707.708L13 12.707l1.146 1.147a.5.5 0 0 0 .708-.708L13.707 12l1.147-1.146a.5.5 0 0 0-.707-.708L13 11.293l-1.146-1.147z" />
-    </svg>
-    <span className="tooltip">Move Note</span>
-  </>
-);
-
-const DeleteIcon = () => (
-  <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={iconSize}
-      height={iconSize}
-      fill="currentColor"
-      class="bi bi-trash"
-      viewBox="0 0 16 16"
-    >
-      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-      <path
-        fill-rule="evenodd"
-        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-      />
-    </svg>
-    <span className="tooltip">Delete Note</span>
-  </>
-);
+import { MoveIcon, ArchiveIcon, DeleteIcon } from "./Icons";
 
 const NoteItem = (props) => {
   return (
